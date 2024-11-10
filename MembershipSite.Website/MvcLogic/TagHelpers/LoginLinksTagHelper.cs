a9ea2ec9e1096ce45c3a9f23e794b3921f099106
Ryan O'Neill <r@ryanoneill.com>
2024-11-10 18:37:08 +0000

    Website: Add login state tag helper for use in theme and main templates.

---
+++ MembershipSite.Website/MvcLogic/TagHelpers/LoginLinksTagHelper.cs
@@ -0,0 +1,22 @@
+﻿namespace MembershipSite.Website.MvcLogic.TagHelpers;
+
+/// <summary>
+/// Transforms HTML markup of the form <mws:loginLinks></mws:loginLinks> or
+/// <mws:loginLinks /> into a login or logout link, depending on the user's authentication state.
+/// </summary>
+/// <param name="httpContextAccessor"></param>
+[HtmlTargetElement("mws:loginLinks", TagStructure = TagStructure.NormalOrSelfClosing)]
+public class LoginLinksTagHelper(IHttpContextAccessor httpContextAccessor) : TagHelper
+{
+    public override void Process(TagHelperContext context, TagHelperOutput output)
+    {
+        var httpContext = httpContextAccessor.HttpContext;
+        var isAuthenticated = httpContext?.User?.Identity?.IsAuthenticated ?? false;
+        var loginStateText = isAuthenticated
+            ? "<a href='/auth/logout'>Logout</a>"
+            : "<a href='/auth/login'>Login</a>";
+
+        output.TagName = null;
+        output.Content.SetHtmlContent(loginStateText);
+    }
+}
