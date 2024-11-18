5cf449e5da7c6288129578fc4d19e7b32478d9c1
Ryan O'Neill <r@ryanoneill.com>
2024-11-18 20:24:25 +0000

    Debug for reading settings from environment.

--- MembershipSite.Website/Controllers/MemberAdminController.cs
+++ MembershipSite.Website/Controllers/MemberAdminController.cs
@@ -1,7 +1,7 @@
 ﻿namespace MembershipSite.Website.Controllers;
 
 [Authorize(Roles = RoleNames.MemberAdmin)]
-public class MemberAdminController(MemberAdminService memberAdminService) : Controller
+public class MemberAdminController(MemberAdminService memberAdminService, AppSettings appSettings) : Controller
 {
     [ActionName("member-list")]
     [Route("member-list", Name = nameof(MemberList))]
@@ -9,6 +9,10 @@ public class MemberAdminController(MemberAdminService memberAdminService) : Cont
     public async Task<IActionResult> MemberList()
     {
         var model = memberAdminService.AllAsQueryable();
+
+        // HACK: For debug. Convert the email contacts to a JSON string for the view.
+        ViewBag.Junk = JsonSerializer.Serialize(appSettings.EmailContacts);
+
         return View(model);
     }
 }
