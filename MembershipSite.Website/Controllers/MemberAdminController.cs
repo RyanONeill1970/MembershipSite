d09ed5e35a60a07c9dcbb16476582e172a896160
Ryan O'Neill <r@ryanoneill.com>
2024-11-18 20:52:09 +0000

    Remove debug messages.

--- MembershipSite.Website/Controllers/MemberAdminController.cs
+++ MembershipSite.Website/Controllers/MemberAdminController.cs
@@ -1,7 +1,7 @@
 ﻿namespace MembershipSite.Website.Controllers;
 
 [Authorize(Roles = RoleNames.MemberAdmin)]
-public class MemberAdminController(MemberAdminService memberAdminService, AppSettings appSettings) : Controller
+public class MemberAdminController(MemberAdminService memberAdminService) : Controller
 {
     [ActionName("member-list")]
     [Route("member-list", Name = nameof(MemberList))]
@@ -9,10 +9,6 @@ public class MemberAdminController(MemberAdminService memberAdminService, AppSet
     public async Task<IActionResult> MemberList()
     {
         var model = memberAdminService.AllAsQueryable();
-
-        // HACK: For debug. Convert the email contacts to a JSON string for the view.
-        ViewBag.Junk = JsonSerializer.Serialize(appSettings.EmailContacts);
-
         return View(model);
     }
 }
