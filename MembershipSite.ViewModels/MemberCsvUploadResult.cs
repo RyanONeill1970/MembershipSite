011f2ae13925007dd2b24a5d680264dfba78f0b6
Ryan O'Neill <r@ryanoneill.com>
2024-12-21 19:05:04 +0000

    Website: Remove nulls from upload result model.

--- MembershipSite.ViewModels/MemberCsvUploadResult.cs
+++ MembershipSite.ViewModels/MemberCsvUploadResult.cs
@@ -6,11 +6,11 @@ public class MemberCsvUploadResult
 
     public List<string> Detail { get; set; } = [];
 
-    public string Error { get; set; }
+    public string Error { get; set; } = "";
 
     public List<string> Failed { get; set; } = [];
 
-    public string Summary { get; set; }
+    public string Summary { get; set; } = "";
 
     public List<string> Updated { get; set; } = [];
 }
