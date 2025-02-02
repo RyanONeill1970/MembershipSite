e4c69502e6e894d810ee2ff4e1c0fd564f65fc71
Ryan O'Neill <r@ryanoneill.com>
2025-02-02 21:52:58 +0000

    Website: Email webhooks - update JSON models.

--- MembershipSite.Logic/Mail/Mailgun/Models/MailgunSignature.cs
+++ MembershipSite.Logic/Mail/Mailgun/Models/MailgunSignature.cs
@@ -2,9 +2,12 @@
 
 public record MailgunSignature
 {
+    [JsonPropertyName("signature")]
     public string Signature { get; set; }
 
+    [JsonPropertyName("timestamp")]
     public string Timestamp { get; set; }
 
+    [JsonPropertyName("token")]
     public string Token { get; set; }
 }
--- MembershipSite.Logic/Mail/Mailgun/Models/MailgunWebhookBase.cs
+++ MembershipSite.Logic/Mail/Mailgun/Models/MailgunWebhookBase.cs
@@ -6,5 +6,63 @@
 /// </summary>
 public record MailgunWebhookBase
 {
+    [JsonPropertyName("signature")]
     public required MailgunSignature Signature { get; set; }
+
+    [JsonPropertyName("eventdata")]
+    public MailgunWebhookEventData EventData { get; set; }
+}
+
+public class MailgunWebhookEventData
+{
+    [JsonPropertyName("id")]
+    public string Id { get; set; }
+
+    [JsonPropertyName("timestamp")]
+    public float Timestamp { get; set; }
+
+    [JsonPropertyName("loglevel")]
+    public string LogLevel { get; set; }
+
+    [JsonPropertyName("event")]
+    public string EventName { get; set; }
+
+    [JsonPropertyName("envelope")]
+    public Envelope Envelope { get; set; }
+
+    [JsonPropertyName("flags")]
+    public Flags Flags { get; set; }
+
+    [JsonPropertyName("message")]
+    public Message Message { get; set; }
+
+    [JsonPropertyName("recipient")]
+    public string Recipient { get; set; }
+
+    [JsonPropertyName("tags")]
+    public List<string> Tags { get; set; }
+
+    [JsonPropertyName("uservariables")]
+    public Dictionary<string, string> UserVariables { get; set; }
+}
+
+public class Envelope
+{
+    [JsonPropertyName("sendingip")]
+    public string SendingIp { get; set; }
+}
+
+public class Flags
+{
+    [JsonPropertyName("istestmode")]
+    public bool IsTestMode { get; set; }
+}
+
+public class Message
+{
+    [JsonPropertyName("headers")]
+    public Dictionary<string, string> Headers { get; set; }
+
+    [JsonPropertyName("size")]
+    public int Size { get; set; }
 }
